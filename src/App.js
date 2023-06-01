b5e1ef734783024fea5dc87ba59c6fcc2ec279a0
unbearabletruth <antaarty@gmail.com>
2023-06-01 17:59:16 +0700

    separate export file

--- src/App.js
+++ src/App.js
@@ -2,40 +2,26 @@ import './assets/styles/App.css';
 import uniqid from "uniqid";
 import React, { useState, useEffect } from "react";
 import Card from './card';
-import Darwin from './assets/images/Darwin.jpg'
-import Dostoevsky from './assets/images/Dostoevky.jpg'
-import Feynman from './assets/images/Feynman.jpg'
-import Solzhenitsyn from './assets/images/Solzhenitsyn.jpg'
-import Dawkins from './assets/images/Dawkins.jpg'
-import Weinstein from './assets/images/Weinstein.jpg'
-import Orwell from './assets/images/Orwell.jpg'
-import Sapolsky from './assets/images/Sapolsky.jpg'
-import de_Waal from './assets/images/de_Waal.jpg'
-import Heying from './assets/images/Heying.jpg'
-import Blaffer_Hrdy from './assets/images/Blaffer_Hrdy.jpg'
-import Frank from './assets/images/Frank.jpg'
-import Rowling from './assets/images/Rowling.jpg'
-import Frankl from './assets/images/Frankl.jpg'
-import Lowry from './assets/images/Lowry.jpg'
+import images from './assets/images/exportAll.js'
 
 function App() {
 
   let [people, setPeople] = useState([
-    {image: Darwin, name:"Charles Darwin", id: uniqid()},
-    {image: Dostoevsky, name: "Fyodor Dostoevsky", id: uniqid()},
-    {image: Feynman, name: "Richard Feynman", id: uniqid()},
-    {image: Solzhenitsyn, name: "Aleksandr Solzhenitsyn", id: uniqid()},
-    {image: Dawkins, name: "Richard Dawkins", id: uniqid()},
-    {image: Weinstein, name: "Bret Weinstein", id: uniqid()},
-    {image: Orwell, name: "George Orwell", id: uniqid()},
-    {image: Sapolsky, name: "Robert Sapolsky", id: uniqid()},
-    {image: de_Waal, name: "Frans de Waal", id: uniqid()},
-    {image: Heying, name: "Heather Heying", id: uniqid()},
-    {image: Blaffer_Hrdy, name: "Sarah Blaffer Hrdy", id: uniqid()},
-    {image: Frank, name: "Anna Frank", id: uniqid()},
-    {image: Rowling, name: "Joanne Rowling", id: uniqid()},
-    {image: Frankl, name: "Victor Frankl", id: uniqid()},
-    {image: Lowry, name: "Lois Lowry", id: uniqid()}
+    {image: images["Darwin.jpg"], name:"Charles Darwin", id: uniqid()},
+    {image: images["Dostoevsky.jpg"], name: "Fyodor Dostoevsky", id: uniqid()},
+    {image: images["Feynman.jpg"], name: "Richard Feynman", id: uniqid()},
+    {image: images["Solzhenitsyn.jpg"], name: "Aleksandr Solzhenitsyn", id: uniqid()},
+    {image: images["Dawkins.jpg"], name: "Richard Dawkins", id: uniqid()},
+    {image: images["Weinstein.jpg"], name: "Bret Weinstein", id: uniqid()},
+    {image: images["Orwell.jpg"], name: "George Orwell", id: uniqid()},
+    {image: images["Sapolsky.jpg"], name: "Robert Sapolsky", id: uniqid()},
+    {image: images["de_Waal.jpg"], name: "Frans de Waal", id: uniqid()},
+    {image: images["Heying.jpg"], name: "Heather Heying", id: uniqid()},
+    {image: images["Blaffer_Hrdy.jpg"], name: "Sarah Blaffer Hrdy", id: uniqid()},
+    {image: images["Frank.jpg"], name: "Anna Frank", id: uniqid()},
+    {image: images["Rowling.jpg"], name: "Joanne Rowling", id: uniqid()},
+    {image: images["Frankl.jpg"], name: "Victor Frankl", id: uniqid()},
+    {image: images["Lowry.jpg"], name: "Lois Lowry", id: uniqid()}
   ])
 
   const [current, setCurrent] = useState(0);
